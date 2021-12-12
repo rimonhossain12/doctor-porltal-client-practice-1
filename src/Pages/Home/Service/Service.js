1010f2fdad71efce6586b453b623e1cdd6c158fd
rimonspi12 <rimonmpi@gmail.com>
2021-12-12 22:16:46 +0600

    appointment banner part code added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
 
-const Service = () => {
+
+
+const Service = (props) => {
+    const {name,img,description} = props.service;
+
     return (
-        <div>
-            <h2>This is single service</h2>
-        </div>
+        <Grid item xs={2} sm={4} md={4}>
+            <Card sx={{ minWidth: 275, border: '0', boxShadow: 0 }}>
+                <CardMedia
+                    component="img"
+                    image={img}
+                    style={{ height: '110', width: 'auto', margin: '0 auto' }}
+                    alt="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        {name}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
     );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import fluoride from '../../../images/fluoride.png';
 import cavity from '../../../images/cavity.png';
 import whitening from '../../../images/whitening.png';
 
+// about import for card section
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
+import Service from '../Service/Service';
 
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
 
 
 const services = [
-    { name: 'Fluoride Treatment', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ornare libero, mattis volutpat lacus. In ut elit nisi. Aliquam et arcu at mauris condimentum maximus. Duis vulputate mattis egestas. Praesent imperdiet urna lectus, at lacinia justo facilisis ac. Proin vitae sem est. Aenean in magna pulvinar purus venenatis auctor in vel lacus. Phasellus sed interdum nisl. Pellentesque id urna in dui scelerisque ornare vitae sed nunc. In placerat at diam quis ornare. Suspendisse rhoncus ornare sem nec consequat. ', img: fluoride },
+    { name: 'Fluoride Treatment', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ornare libero, mattis volutpat lacus. In ut elit nisi. Aliquam et arcu at mauris condimentum maximus. Duis vulputate mattis egestas', img: fluoride },
 
-    { name: 'Cavity Filling', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ornare libero, mattis volutpat lacus. In ut elit nisi. Aliquam et arcu at mauris condimentum maximus. Duis vulputate mattis egestas. Praesent imperdiet urna lectus, at lacinia justo facilisis ac. Proin vitae sem est. Aenean in magna pulvinar purus venenatis auctor in vel lacus. Phasellus sed interdum nisl. Pellentesque id urna in dui scelerisque ornare vitae sed nunc. In placerat at diam quis ornare. Suspendisse rhoncus ornare sem nec consequat.', img: cavity },
-    { name: 'Teath Whitening', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ornare libero, mattis volutpat lacus. In ut elit nisi. Aliquam et arcu at mauris condimentum maximus. Duis vulputate mattis egestas. Praesent imperdiet urna lectus, at lacinia justo facilisis ac. Proin vitae sem est. Aenean in magna pulvinar purus venenatis auctor in vel lacus. Phasellus sed interdum nisl. Pellentesque id urna in dui scelerisque ornare vitae sed nunc. In placerat at diam quis ornare. Suspendisse rhoncus ornare sem nec consequat.', img: whitening }
+    { name: 'Cavity Filling', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ornare libero, mattis volutpat lacus. In ut elit nisi. Aliquam et arcu at mauris condimentum maximus. Duis vulputate mattis egestas', img: cavity },
+    { name: 'Teath Whitening', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ornare libero, mattis volutpat lacus. In ut elit nisi. Aliquam et arcu at mauris condimentum maximus. Duis vulputate mattis egestas', img: whitening }
 ]
 
 const Services = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {Array.from(Array(6)).map((_, index) => (
-                    <Grid item xs={2} sm={4} md={4} key={index}>
-                        <Item>xs=2</Item>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
+        <Container>
+            <Box sx={{ flexGrow: 1 }}>
+                <Typography variant="h5" component="div" style={{ color:'#0EACB0'}} sx={{ fontWeight: '400',m:3}}>
+                    OUR SERVICE
+                </Typography>
+                <Typography variant="h5" sx={{ fontWeight: '600',m:5 }} component="div">
+                   Services We Provide
+                </Typography>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                 {
+                        services.map(service => <Service key={service.name}
+                         service={service}
+                     ></Service>)
+                 }
+                </Grid>
+            </Box>
+        </Container>
     );
 };
 
