04685bd8774d43f96bd5803c4cf7cec058c61aa4
rimonspi12 <rimonmpi@gmail.com>
2021-12-16 10:32:07 +0600

    dental care section is added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DentalCare from '../../DentalCare/DentalCare';
 import Navigation from '../../Shared/Navigation/Navigation';
 import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
 import Banner from '../Banner/Banner';
@@ -10,6 +11,7 @@ const Home = () => {
             <Navigation></Navigation>
             <Banner></Banner>
             <Services></Services>
+            <DentalCare></DentalCare>
             <AppointmentBanner></AppointmentBanner>
            
         </div>
